af1b1de7f909906a38dc58ecdeb9343dc0281dd1
clay <claycampbell@gmail.com>
2023-06-01 13:27:07 -0700

    Update main_st.py

--- main_st.py
+++ main_st.py
@@ -20,26 +20,22 @@ import json
 
 # Function to select roles
 def select_role(role_type, roles):
-    selected_role = st.selectbox(f"Select {role_type} role:", ["Custom Role"] + roles)
+    selected_role = st.selectbox(f"Select {role_type} role:",  ["Custom Role"] + roles )
     if selected_role == "Custom Role":
         custom_role = st.text_input(f"Enter the {role_type} (Custom Role):")
         return custom_role
     else:
         return selected_role
 
-
 # Function to get system messages for AI assistant and AI user from role names and the task
 def get_sys_msgs(assistant_role_name: str, user_role_name: str, task: str):
     assistant_sys_template = SystemMessagePromptTemplate.from_template(template=assistant_inception_prompt)
-    assistant_sys_msg = assistant_sys_template.format_messages(assistant_role_name=assistant_role_name,
-                                                               user_role_name=user_role_name, task=task)[0]
+    assistant_sys_msg = assistant_sys_template.format_messages(assistant_role_name=assistant_role_name, user_role_name=user_role_name, task=task)[0]
 
     user_sys_template = SystemMessagePromptTemplate.from_template(template=user_inception_prompt)
-    user_sys_msg = user_sys_template.format_messages(assistant_role_name=assistant_role_name,
-                                                     user_role_name=user_role_name, task=task)[0]
+    user_sys_msg = user_sys_template.format_messages(assistant_role_name=assistant_role_name, user_role_name=user_role_name, task=task)[0]
 
     return assistant_sys_msg, user_sys_msg
-
 
 def generate_unique_task_name(task: str, chat_history_items: List[dict]) -> str:
     task_name = task
@@ -51,7 +47,6 @@ def generate_unique_task_name(task: str, chat_history_items: List[dict]) -> str:
         count += 1
 
     return task_name
-
 
 def load_chat_history_items() -> List[dict]:
     chat_history_items = []
@@ -68,198 +63,204 @@ def load_chat_history_items() -> List[dict]:
 chat_history_items = []
 
 
-st.set_page_config(layout="centered")
+st.set_page_config(layout="centered") 
 
-st.title("OmniSolver 🔆",
-         help="This app uses the CAMEL framework to solve problems. This app uses GPT models and the responses may not be accurate")
+st.title("OmniSolver 🔆", help="This app uses the CAMEL framework to solve problems. This app uses GPT models and the responses may not be accurate")
 
 # Sidebar: API Key input
 st.sidebar.title("Configuration")
 # comment this out if you want to use the API key from the environment variable locally
-api_key = st.sidebar.text_input("Enter your OpenAI API Key:", type="password")
+#api_key = st.sidebar.text_input("Enter your OpenAI API Key:", type="password")
 
 # uncomment this if you want to use the API key from the environment variable locally
-# api_key = os.getenv("OPENAI_API_KEY")
+api_key = os.getenv("OPENAI_API_KEY")
 
 if api_key:
     os.environ["OPENAI_API_KEY"] = api_key
 elif api_key == "":
     st.sidebar.warning("Please enter your OpenAI API Key.")
 
+  
 
 # Sidebar: Model selection
 model = st.sidebar.radio("Select the model:", ("gpt-3.5-turbo", "gpt-4"))
 
 with open("stats.txt", "r") as stats_file:
-    stats = stats_file.readlines()
-    tasks_solved = int(stats[0].strip())
-    tasks_solved += 1
-    st.write(f"<p style='color: green; font-weight: bold;'>This App was used to solve *{tasks_solved}* tasks so far since deployed</p>",
-             unsafe_allow_html=True)
-    stats[0] = str(tasks_solved) + "\n"
-    with open("stats.txt", "w") as stats_file_write:
-        stats_file_write.writelines(stats)
+                    stats = stats_file.readlines()
+                    tasks_solved = int(stats[0].strip())
+                    tasks_solved += 1
+                    st.write(f"<p style='color: green; font-weight: bold;'>This App was used to solve *{tasks_solved}* tasks so far since deployed</p>", unsafe_allow_html=True)
+# Main: Load roles from roles.txt
+with open("roles.txt", "r") as roles_file:
+    roles_list = [line.strip() for line in roles_file.readlines()]
 
-if model == "gpt-4":
-    st.warning("The GPT-4 model is currently not available. Please select GPT-3.5 Turbo.")
-    st.stop()
+# Main: Role selection
+user_role_name = select_role("AI user", roles_list)
+assistant_role_name = select_role("AI assistant", roles_list)
 
-# Load the chat history items
-chat_history_items = load_chat_history_items()
 
-# Chat history list
-chat_history = []
+if assistant_role_name and user_role_name:
+    # Main: Task input
+    task = st.text_input("Please enter the task:")
 
-# Get the assistant and user role names
-assistant_role_name = select_role("Assistant", ["Assistant"])
-user_role_name = select_role("User", ["User"])
+    if task:
+        # Main: Task specifier
+        task_specifier = st.checkbox("Do you want to use the task specifier feature?", help="Use the task specifier feature to make a task more specific by GPT. May not work as expected.")
 
-# Get the task name from the user
-task = st.text_input("Enter the task you want to solve:", key="task_input")
+        if task_specifier:
+            word_limit = st.number_input("Please enter the word limit for the specified task:", min_value=1, value=50, step=1)
 
-if task:
-    task_name = generate_unique_task_name(task, chat_history_items)
-    st.info(f"Task: {task_name}")
+            if word_limit:
+                task_specifier_sys_msg = SystemMessage(content="You can make a task more specific.")
+                task_specifier_prompt = (
+                    """Here is a task that {assistant_role_name} will help {user_role_name} to complete: {task}.
+                    Please make it more specific. Be creative and imaginative.
+                    Please reply with the specified task in {word_limit} words or less. Do not add anything else."""
+                )
+                task_specifier_template = HumanMessagePromptTemplate.from_template(template=task_specifier_prompt)
+                task_specify_agent = CAMELAgent(task_specifier_sys_msg, ChatOpenAI(model=model, temperature=1.0))
+                task_specifier_msg = task_specifier_template.format_messages(assistant_role_name=assistant_role_name,
+                                                            user_role_name=user_role_name,
+                                                            task=task, word_limit=word_limit)[0]
+                specified_task_msg = task_specify_agent.step(task_specifier_msg)
+                st.write(f"<p style='font-weight: bold;'>Specified task:</p> {specified_task_msg.content}", unsafe_allow_html=True)
 
-    # Get system messages for AI assistant and AI user
-    assistant_sys_msg, user_sys_msg = get_sys_msgs(assistant_role_name, user_role_name, task_name)
-
-    # Create the AI assistant agent
-    assistant_agent = CAMELAgent()
-
-    # Start the conversation
-    if st.button("Start"):
-        chat_history.append(assistant_sys_msg)
-        chat_history.append(user_sys_msg)
-
-    # Conversation loop
-    while True:
-        # Display the chat history
-        for item in chat_history:
-            role = item["role"]
-            content = item["content"]
-            if role == assistant_role_name:
-                st.text_area(role, content, key=f"{role}_history", height=200)
-            else:
-                st.text_area(role, content, key=f"{role}_history", height=100)
-
-        # User input
-        user_input = st.text_input(f"{user_role_name}:",
-                                   help="Enter your message here and press Enter to send.", key="user_input")
-
-        if st.button("Send") or user_input.endswith("\n"):
-            # Add user message to chat history
-            user_msg = HumanMessage(content=user_input.strip())
-            chat_history.append(user_msg)
-
-            # Reset conversation agents
-            assistant_agent.reset()
-
-            # Send user message to assistant agent
-            assistant_msg = assistant_agent.step(user_msg)
-            chat_history.append(assistant_msg)
-
-            # Clear user input
-            user_input = ""
-
-        # Check if there is an uploaded file
-        uploaded_file = st.file_uploader("Upload a document")
-
-        # Handle the uploaded document
-        if uploaded_file is not None:
-            document_contents = uploaded_file.read().decode("utf-8")
+                specified_task = specified_task_msg.content
         else:
-            document_contents = ""
+            specified_task = task
 
-        # Include document in the conversation
-        if document_contents:
-            document_sys_msg = SystemMessage(content="Here is the document we will be working from:")
-            assistant_msg = assistant_agent.step(document_sys_msg)
-            user_msg = HumanMessage(content=document_contents)
-            user_msg = assistant_agent.step(user_msg)
-            chat_history.append({"role": "Document", "content": document_contents})
+        if specified_task:
+            # Main: Chat turn limit input
+            chat_turn_limit = st.number_input("Please enter the chat turn limit:", min_value=1, step=1)
+            
+            if st.button("Start Solving Task"):
+                if api_key == "":
+                    st.warning("Please enter your OpenAI API Key.")
+                    st.stop()
 
-        # Continue with the existing code for the conversation loop
-        if user_input.strip() == "":
-            continue
+                with open("stats.txt", "w") as stats_file:
+                    stats_file.write(str(tasks_solved))
 
-        # AI User message
-        user_msg = HumanMessage(content=user_input)
-        chat_history.append(user_msg)
+                chat_history_items = load_chat_history_items()
+                with st.spinner("Thinking..."):
+                    # Main: Initialize agents and start role-playing session
+                    assistant_sys_msg, user_sys_msg = get_sys_msgs(assistant_role_name, user_role_name, specified_task)
+                    assistant_agent = CAMELAgent(assistant_sys_msg, ChatOpenAI(model=model, temperature=0.2))
+                    user_agent = CAMELAgent(user_sys_msg, ChatOpenAI(model=model, temperature=0.2))
 
-        # Reset conversation agents
-        assistant_agent.reset()
+                    assistant_agent.reset()
+                    user_agent.reset()
 
-        # AI User agent step
-        assistant_msg = assistant_agent.step(user_msg)
-        chat_history.append(assistant_msg)
+                    assistant_msg = HumanMessage(
+                        content=(f"{user_sys_msg.content}. "
+                                "Now start to give me introductions one by one. "
+                                "Only reply with Instruction and Input."))
 
-else:
-    st.info("Enter a task to start the conversation.")
+                    user_msg = HumanMessage(content=f"{assistant_sys_msg.content}")
+                    user_msg = assistant_agent.step(user_msg)
 
-# Main: Save chat history to file
-task_name = generate_unique_task_name(task, chat_history_items)
-history_dict = {
-    "task": task_name,
-    "settings": {
-        "assistant_role_name": assistant_role_name,
-        "user_role_name": user_role_name,
-        "model": model,
-        "chat_turn_limit": chat_turn_limit,
-    },
-    "conversation": chat_history,
-}
+                    st.write(f"<p style='color: red;'><b>Original task prompt:</b></p>\n\n{task}\n", unsafe_allow_html=True)
+                    st.write(f"<p style='color: red;'><b>Specified task prompt:</b></p>\n\n{specified_task}\n", unsafe_allow_html=True)
 
-with open("chat_history.json", "a") as history_file:
-    json.dump(history_dict, history_file)
-    history_file.write("\n")
 
-if not task:
-    st.warning("Please enter the task.")
-elif not assistant_role_name or not user_role_name:
-    st.warning("Please select both AI assistant and AI user roles.")
-else:
-    # Sidebar: Load chat history
-    chat_history_titles = [item["task"] for item in chat_history_items]
-    try:
-        chat_history_items = load_chat_history_items()
-        chat_history_titles = [item["task"] for item in chat_history_items]
-        selected_history = st.sidebar.selectbox("Select chat history:", ["None"] + chat_history_titles)
+                    chat_history = []
 
-        if selected_history != "None":
-            delete_history_button = st.sidebar.button("Delete Selected Chat History")
+                    with st.spinner("Running role-playing session to solve the task..."):
+                        # Replace the for loop with the following code:
+                        progress = st.progress(0)
+                        for n in range(chat_turn_limit):
+                            user_ai_msg = user_agent.step(assistant_msg)
+                            user_msg = HumanMessage(content=user_ai_msg.content)
 
-            if delete_history_button and selected_history != "None":
-                chat_history_items.pop(chat_history_titles.index(selected_history))
+                            chat_history.append({"role": user_role_name, "content": user_msg.content})
+                            st.markdown(f"<p style='color: blue; font-weight: bold;'>{user_role_name}</p>\n\n{user_msg.content}\n\n", unsafe_allow_html=True)
 
-                # Save the updated chat history to file
-                with open("chat_history.json", "w") as history_file:
-                    for item in chat_history_items:
-                        json.dump(item, history_file)
+                            assistant_ai_msg = assistant_agent.step(user_msg)
+                            assistant_msg = HumanMessage(content=assistant_ai_msg.content)
+
+                            chat_history.append({"role": assistant_role_name, "content": assistant_msg.content})
+                            st.markdown(f"<p style='color: green; font-weight: bold;'>{assistant_role_name}</p>\n\n{assistant_msg.content}\n\n", unsafe_allow_html=True)
+
+                            progress.progress((n+1)/chat_turn_limit)
+
+                            if "<CAMEL_TASK_DONE>" in user_msg.content:
+                                break
+
+                        progress.empty()
+
+
+                    
+
+                    # Main: Save chat history to file
+                    task_name = generate_unique_task_name(task, chat_history_items)
+                    history_dict = {
+                        "task": task_name,
+                        "settings": {
+                            "assistant_role_name": assistant_role_name,
+                            "user_role_name": user_role_name,
+                            "model": model,
+                            "chat_turn_limit": chat_turn_limit,
+                        },
+                        "conversation": chat_history,
+                    }
+
+                    with open("chat_history.json", "a") as history_file:
+                        json.dump(history_dict, history_file)
                         history_file.write("\n")
+                    
 
-                st.sidebar.success("Selected chat history deleted.")
-                st.experimental_rerun()
+            else:
+                st.warning("Please enter the chat turn limit.")
+        else:
+            st.warning("Please specify the task.")
+    else:
+        st.warning("Please enter the task.")
+else:
+    st.warning("Please select both AI assistant and AI user roles.")
 
-        # Main: Display selected chat history
-        if selected_history != "None":
-            selected_history_item = chat_history_items[chat_history_titles.index(selected_history)]
-            settings = selected_history_item["settings"]
-            conversation = selected_history_item["conversation"]
+# Sidebar: Load chat history
+chat_history_titles = [item["task"] for item in chat_history_items]
+try:
+    chat_history_items = load_chat_history_items()
+    chat_history_titles = [item["task"] for item in chat_history_items]
+    selected_history = st.sidebar.selectbox("Select chat history:", ["None"] + chat_history_titles)
 
-            st.write(f"<p style='color: green; font-weight: bold;'>Task:</p> {selected_history}\n", unsafe_allow_html=True)
+    if selected_history != "None":
+        delete_history_button = st.sidebar.button("Delete Selected Chat History")
 
-            st.write(f"""<p style='color: green; font-weight: bold;'>Settings:</p>
-                        <p>- AI assistant role: <span >{settings['assistant_role_name']}</span></p>
-                        <p>- AI user role: <span >{settings['user_role_name']}</span></p>
-                        <p>- Model: {settings['model']}</p>
-                        <p>- Chat turn limit: {settings['chat_turn_limit']}</p>
-                        """, unsafe_allow_html=True)
+        if delete_history_button and selected_history != "None":
+            chat_history_items.pop(chat_history_titles.index(selected_history))
 
-            for msg in conversation:
-                st.markdown(f"<p style='color: green; font-weight: bold;'>{msg['role']}</p>\n\n{msg['content']}\n\n", unsafe_allow_html=True)
+            # Save the updated chat history to file
+            with open("chat_history.json", "w") as history_file:
+                for item in chat_history_items:
+                    json.dump(item, history_file)
+                    history_file.write("\n")
 
-    except FileNotFoundError:
-        st.sidebar.warning("No chat history available.")
+            st.sidebar.success("Selected chat history deleted.")
+            st.experimental_rerun()
 
+
+
+    # Main: Display selected chat history
+    if selected_history != "None":
+        selected_history_item = chat_history_items[chat_history_titles.index(selected_history)]
+        settings = selected_history_item["settings"]
+        conversation = selected_history_item["conversation"]
+
+        st.write(f"<p style='color: green; font-weight: bold;'>Task:</p> {selected_history}\n", unsafe_allow_html=True)
+
+        st.write(f"""<p style='color: green; font-weight: bold;'>Settings:</p>
+                    <p>- AI assistant role: <span >{settings['assistant_role_name']}</span></p>
+                    <p>- AI user role: <span >{settings['user_role_name']}</span></p>
+                    <p>- Model: {settings['model']}</p>
+                    <p>- Chat turn limit: {settings['chat_turn_limit']}</p>
+                    """, unsafe_allow_html=True)
+
+
+        for msg in conversation:
+            st.markdown(f"<p style='color: green; font-weight: bold;'>{msg['role']}</p>\n\n{msg['content']}\n\n", unsafe_allow_html=True)
+
+except FileNotFoundError:
+    st.sidebar.warning("No chat history available.")
 
